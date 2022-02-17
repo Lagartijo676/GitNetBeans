e89954519d62e7a509347e8b5083e153db891475
Miguel <maalivent@gmail.com>
2022-02-17 14:03:42 +0100

    Modificando desde la rama circulo

--- src/circulo/Circulo.java
+++ src/circulo/Circulo.java
@@ -36,7 +36,7 @@ public class Circulo {
     }
     public static void CalculaLongitud(double r) {
         double area=(PI*r*r);
-	System.out.printf("El valor del área es %f\n",area);
+	System.out.printf("El vralor del área es %f\n",area);
     }
     
    
